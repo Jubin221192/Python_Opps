aaededa14e2fecc4cac679b4c25b2bf94ec98b81
Jubin221192 <mohantyjubin2211@gmail.com>
2019-08-16 22:42:06 -0400

    commit new changes

---
+++ Dunder_Methods.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 18:44:27 2019
+
+@author: jubin
+"""
+
---
+++ Linked_List_Coding_Challenges.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 22:18:25 2019
+
+@author: jubin
+"""
+
+# Linked List Implementation
+
+class Node(object):
+    
+    def __init__(self,value, nextnode = None):
+        self.value= value
+        self.nextnode= nextnode
+        
+    def __str__(self):
+        return str(self.value)
+
+    
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+
+a.nextnode = b
+b.nextnode = c
+
+a.value
+
+b.value
+
+c.value
+
+# printing the linked list in readable format
+def linked_list_to_string(head):
+    current = head
+    str_list = []
+    while current:
+        str_list.append(str(current.value))
+        current = current.nextnode
+    str_list.append('(None)')
+    return ' -> '.join(str_list)
+
+# inserting values
+current = Node(1)
+for i in range(2, 8):
+    current = Node(i, current)
+head = current
+
+linked_list_to_string(head)
+
+# Finding the nth element
+def nth_from_last(head, n):
+    left = head
+    right = head
+    for i in range(n):
+        if right == None:
+            return None
+        right = right.nextnode
+    while right:
+        right = right.nextnode
+        left = left.nextnode
+    return left
+
+# Finding the nth element
+print(nth_from_last(head,2))
+
+
